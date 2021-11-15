1806ef694627da2ac2a26725fcfc3c4a9f080153
gongjs <gongjiansheng@agora.io>
2021-11-15 11:51:31 +0800

    feat: 命令注册

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -7,10 +7,12 @@ import pathExists from 'path-exists'
 import path from 'path'
 import dotenv from 'dotenv'
 import minimist from 'minimist'
+import commander from 'commander'
 import { LOWEST_NODE_VERSION, DEFAULT_CLI_HOME } from '../../../const'
 
 const pkg = require('../package.json')
 const userHome = os.homedir()
+const program = new commander.Command()
 
 const core = () => {
     try {
@@ -21,6 +23,7 @@ const core = () => {
         checkInputArgs()
         checkEnv()
         checkGlobalUpdate()
+        registerCommander()
     }  catch (e) {
         if (e instanceof Error) {
             log.error('core', e.message)
@@ -88,6 +91,50 @@ const checkGlobalUpdate = async () => {
     const latestVersion = await http.getNpmSemverVersion(currentVersion, npmName)
     if (latestVersion && semver.gt(latestVersion, currentVersion)) {
         log.warn('checkGlobalUpdate', colors.yellow(`请手动更新 ${npmName}, 当前版本：${currentVersion}, 最新版本；${latestVersion}`))
+    }
+}
+
+const registerCommander = () => {
+    program
+        .name(Object.keys(pkg.bin)[0])
+        .usage('<command> [options]')
+        .version(pkg.version)
+        .option('-d, --debug', '是否开启调试模式', false)
+        .option('-tp, --targetPath <targetPath>', '是否指定本地调试文件路径', '');
+
+    program
+        .command('init [projectName]')
+        .option('-f, --force', '是否强制初始化项目', false)
+        .action(() => {
+            console.log('action')
+        })
+
+    program.on('option:targetPath', function() {
+       process.env.CLI_TARGET_PATH = program.opts().targetPath
+    })
+
+    program.on('option:debug', function() {
+        if (program.opts().debug) {
+            process.env.LOG_LEVEL = 'verbose'
+        } else {
+            process.env.LOG_LEVEL = 'info'
+        }
+        log.level = process.env.LOG_LEVEL
+        log.verbose('debug', '开启 debug 模式')
+    })
+
+    program.on('command:*', function(obj) {
+        const availableCommands = program.commands.map(cmd => cmd.name())
+        console.log(colors.red('未知命令' + obj[0]))
+        if (availableCommands.length > 0) {
+            console.log(colors.red('可用命令' + availableCommands.join(',')))
+        }
+    })
+
+    program.parse(process.argv);
+    
+    if (program.args && program.args.length < 1) {
+        program.outputHelp()
     }
 }
 export default core
